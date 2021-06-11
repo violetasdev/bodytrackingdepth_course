3b8c66bfc8224675e0d0688c8790f4abfc521cca
Violeta Sosa <vsosa.ud@gmail.com>
2021-06-11 06:43:32 +0200

    Exercise 01
    Kinect V2 first exercise code

---
+++ KinectV2/kinectTutorial01/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// Kinect V2 Extensions
+using Microsoft.Kinect;
+
+/***
+Body Tracking with depth sensor course SS2021
+Kinect Tutorial 01: Access the sensor
+
+Goal: Get access to the Kinect by adding the libraries
+Result: An empty Window and the Kinect V2 lights on. We still havent call the feed into the interface!
+**/
+namespace kinectTutorial01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Create the Kinect sensor object
+        private KinectSensor kinectSensor = null;
+        public MainWindow()
+        {
+            // Initialize the sensor
+            kinectSensor = KinectSensor.GetDefault();
+            // Open the sensor
+            kinectSensor.Open();
+            InitializeComponent();
+        }
+    }
+}
